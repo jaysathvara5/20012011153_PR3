63c4413d0374412e96904800f6b7dedf69e8df5a
jaysa <jay2003kadiya@gmail.com>
2022-11-18 22:01:16 +0530

    PR3

---
+++ app/src/main/java/com/example/pr3/ActivityMainBinding.kt
@@ -0,0 +1,5 @@
+package com.example.pr3
+
+class ActivityMainBinding {
+
+}
---
+++ app/src/main/java/com/example/pr3/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.example.pr3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.res.ComplexColorCompat.inflate
+import com.example.pr3.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.bottomNavigationView.selectedItemId = R.id.button_nav_layout
+        binding.bottomNavigationView.setOnItemSelectedListener{it1->
+            when(it1.itemId)
+            {
+                R.id.button_nav_reg->
+                {
+                    Intent(this,RegistrationActivity::class.java).also {startActivity(it)}
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+        binding.tvSignup.setOnClickListener{
+            Intent(this,RegistrationActivity::class.java).apply {
+                startActivity(this)
+            }
+
+        }
+    }
+}
